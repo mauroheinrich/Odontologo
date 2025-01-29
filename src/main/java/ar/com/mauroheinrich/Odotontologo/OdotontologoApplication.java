507997f001b063746b596108a4395686008deabf
Mauro <heinrichmauro@gmail.com>
2025-01-29 08:42:49 -0300

    primer commit

---
+++ src/main/java/ar/com/mauroheinrich/Odotontologo/OdotontologoApplication.java
@@ -0,0 +1,13 @@
+package ar.com.mauroheinrich.Odotontologo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdotontologoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdotontologoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/com/mauroheinrich/Odotontologo/OdotontologoApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.mauroheinrich.Odotontologo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdotontologoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
